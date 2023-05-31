6e34783ed51d2b40f0b165bdbcc36aa72177fb33
xiaoguang <zhxiaog@outlook.com>
2023-05-31 07:12:22 -0700

    remove back slash when handling requests

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,10 @@ use std::sync::Arc;
 mod handlers;
 mod protos;
 mod utils;
+use axum::ServiceExt;
 
 use axum::routing::get;
-use axum::{Router};
+use axum::Router;
 use constdb::{Engine, Settings};
 use handlers::database::db_routes;
 use handlers::dml::dml_routes;
@@ -16,6 +17,8 @@ use handlers::table::table_routes;
 use tokio::sync::RwLock;
 
 use clap::Parser;
+use tower::layer::Layer;
+use tower_http::normalize_path::NormalizePathLayer;
 
 /// The constdb app
 #[derive(Debug, Parser)]
@@ -34,15 +37,14 @@ async fn main() {
     };
     let const_db = Arc::new(RwLock::new(Engine::new(settings).unwrap()));
 
-    let app = Router::new()
+    let router = Router::new()
         .route("/", get(root))
-        .nest("/api/v1/dbs/", db_routes())
-        .nest("/api/v1/dbs/:db_name/tables/", table_routes())
-        .nest(
-            "/api/v1/dbs/:db_name/tables/:table_name/data/",
-            dml_routes(),
-        )
+        .nest("/api/v1/dbs", db_routes())
+        .nest("/api/v1/dbs/:db_name/tables", table_routes())
+        .nest("/api/v1/dbs/:db_name/tables/:table_name/data", dml_routes())
         .with_state(const_db);
+
+    let app = NormalizePathLayer::trim_trailing_slash().layer(router);
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     axum::Server::bind(&addr)
